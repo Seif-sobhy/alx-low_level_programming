01c0358e1d1df414aa91191f5ea52745458e0dba
seif sobhy <seifsobhy5@gmail.com>
2023-06-15 15:40:15 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(void)
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put functiom
+ *
+ * Return: Alawys 0 (success)
+ */
+
+int  main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
