c6a6841ec1bb5ac6e5729956717c55feda3120a4
seif sobhy <seifsobhy5@gmail.com>
2023-07-10 04:54:17 -0700

    a

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *arr;
+	char* arr;
 	unsigned int i;
 
 	arr = malloc(sizeof(char) * size);
@@ -26,5 +26,5 @@ char *create_array(unsigned int size, char c)
 		arr[i] = c;
 	}
 
-	return (arr[i]);
+	return (arr);
 }
