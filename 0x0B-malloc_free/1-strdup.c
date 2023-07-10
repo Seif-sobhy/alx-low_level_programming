7fd9388d819050ffbe8448c7d7d79e9987d19e80
seif sobhy <seifsobhy5@gmail.com>
2023-07-10 05:43:42 -0700

    a

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strdup - Main Entry
+ * @str: input
+ * Return: 0
+*/
+
+char *_strdup(char *str)
+{
+	char *newstr;
+	unsigned int i, length;
+
+	if (str == 0 || newstr == 0)
+	{
+	return (NULL);
+	}
+
+	length = 0;
+	while (str[length] != '\0')
+	{
+	length++;
+	}
+
+	newstr = malloc(sizeof(char) * (length + 1));
+
+	for (i = 0; i < length; i++)
+	{
+	newstr[i] = str[i];
+	}
+
+	newstr[length] = '\0';
+
+	return (newstr);
+
+}
